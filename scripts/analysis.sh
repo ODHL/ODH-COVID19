da85aecea13f25c23993ebb923b7bab2aea730f9
slsevilla <slsevilla@gmail.com>
2023-12-09 03:49:06 +0000

    feat: handle proj failures with multiple sample repeats

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -373,14 +373,18 @@ if [[ $flag_analysis == "Y" ]]; then
 				$config_basespace_cmd download biosample --quiet -n "${sample_id}" -o $rawdata_dir
 
 				# move files to batch fasta dir
-				mv $rawdata_dir/*${sample_id}*/*fastq.gz $fastq_batch_dir
+				## using head to only move the first file - when re-runs happen more than one file
+				## may be associated to the project
+				mv `ls $rawdata_dir/*${sample_id}*/*R1*fastq.gz | head -1` $fastq_batch_dir
+				mv `ls $rawdata_dir/*${sample_id}*/*R2*fastq.gz | head -1` $fastq_batch_dir
 		
 				# If generating a QC report, BASESPACE files need to be unzipped
 				# and selected files moved for downstream analysis
-				if [[ ! -d "$tmp_dir/${sample_id}" ]]; then mkdir $tmp_dir/${sample_id}; fi
-				
+				if [[ ! -d "$tmp_dir/${sample_id}" ]]; then mkdir -p $tmp_dir/${sample_id}/hold; fi
+				mv `ls $tmp_dir/${sample_id}*/*.zip | head -1` $tmp_dir/${sample_id}/hold
+
 				# unzip analysis file downloaded from DRAGEN to sample tmp dir - used in QC
-				unzip -o -q $tmp_dir/${sample_id}*/*.zip -d $tmp_dir/${sample_id}
+				unzip -o -q $tmp_dir/${sample_id}*/hold/*.zip -d $tmp_dir/${sample_id}
 
 				# move needed files to general tmp dir
 				mv $tmp_dir/${sample_id}/ma/* $tmp_dir/unzipped
@@ -392,85 +396,85 @@ if [[ $flag_analysis == "Y" ]]; then
 				rm -r --force $tmp_dir/${sample_id}_[0-9]*/
 			done
 			
-			# remove the "_S39_L001" and "_001" from the file name
-			for f in $fastq_batch_dir/*; do
-				new=`echo $f | sed "s/_S[0-9].*_L001//g" | sed "s/_001//g" | sed "s/[_-]SARS//g" | sed "s/-$project_name_full//g" | sed "s/-$project_name//g" | sed "s/_R/.R/g"`
-				if [[ $new != $f ]]; then mv $f $new; fi
-			done
+			# # remove the "_S39_L001" and "_001" from the file name
+			# for f in $fastq_batch_dir/*; do
+			# 	new=`echo $f | sed "s/_S[0-9].*_L001//g" | sed "s/_001//g" | sed "s/[_-]SARS//g" | sed "s/-$project_name_full//g" | sed "s/-$project_name//g" | sed "s/_R/.R/g"`
+			# 	if [[ $new != $f ]]; then mv $f $new; fi
+			# done
 
-			# rename all ID files
-			## batch manifests
-			sed -i "s/[_-]SARS//g" $batch_manifest; sed -i "s/-$project_name_full//g" $batch_manifest
-			sed -i "s/-$project_name//g" $batch_manifest
-			sed -i "s/[_-]SARS//g" $samplesheet; sed -i "s/-$project_name_full//g" $samplesheet
-			sed -i "s/-$project_name//g" $samplesheet
+			# # rename all ID files
+			# ## batch manifests
+			# sed -i "s/[_-]SARS//g" $batch_manifest; sed -i "s/-$project_name_full//g" $batch_manifest
+			# sed -i "s/-$project_name//g" $batch_manifest
+			# sed -i "s/[_-]SARS//g" $samplesheet; sed -i "s/-$project_name_full//g" $samplesheet
+			# sed -i "s/-$project_name//g" $samplesheet
 			
-			## qc files renamed
-			for f in $tmp_dir/unzipped/*; do
-				new=`echo $f | sed "s/[_-]SARS//g" | sed "s/-$project_name_full//g" | sed "s/-$project_name//g"`
-				if [[ $new != $f ]]; then mv $f $new; fi
-			done
+			# ## qc files renamed
+			# for f in $tmp_dir/unzipped/*; do
+			# 	new=`echo $f | sed "s/[_-]SARS//g" | sed "s/-$project_name_full//g" | sed "s/-$project_name//g"`
+			# 	if [[ $new != $f ]]; then mv $f $new; fi
+			# done
 
-			#log
-			message_cmd_log "------CECRET"
-			echo "-------Starting time: `date`" >> $pipeline_log
-			echo "-------Starting space: `df . | sed -n '2 p' | awk '{print $5}'`" >> $pipeline_log
+			# #log
+			# message_cmd_log "------CECRET"
+			# echo "-------Starting time: `date`" >> $pipeline_log
+			# echo "-------Starting space: `df . | sed -n '2 p' | awk '{print $5}'`" >> $pipeline_log
 		
-			# copy config
-			cp $cecret_config $pipeline_batch_dir
+			# # copy config
+			# cp $cecret_config $pipeline_batch_dir
 
-			# deploy cecret
-			cd $project_number
-			cecret_cmd_line="$cecret_cmd --sample_sheet $samplesheet --reads_type paired --outdir $pipeline_batch_dir"
-			echo $cecret_cmd_line
-			$cecret_cmd_line
+			# # deploy cecret
+			# cd $project_number
+			# cecret_cmd_line="$cecret_cmd --sample_sheet $samplesheet --reads_type paired --outdir $pipeline_batch_dir"
+			# echo $cecret_cmd_line
+			# $cecret_cmd_line
 		fi
 		
-		# log
-    	echo "-------Ending time: `date`" >> $pipeline_log
-		echo "-------Ending space: `df . | sed -n '2 p' | awk '{print $5}'`" >> $pipeline_log
+		# # log
+    	# echo "-------Ending time: `date`" >> $pipeline_log
+		# echo "-------Ending space: `df . | sed -n '2 p' | awk '{print $5}'`" >> $pipeline_log
 
-		#############################################################################################
-		# Reporting
-		#############################################################################################	
-		# add to  master cecret results
-		cat $pipeline_batch_dir/cecret_results.txt >> $merged_cecret
+		# #############################################################################################
+		# # Reporting
+		# #############################################################################################	
+		# # add to  master cecret results
+		# cat $pipeline_batch_dir/cecret_results.txt >> $merged_cecret
 
-		# add to master nextclade results
-		cat $pipeline_batch_dir/nextclade/nextclade.csv >> $merged_nextclade
+		# # add to master nextclade results
+		# cat $pipeline_batch_dir/nextclade/nextclade.csv >> $merged_nextclade
 
-	    # add to master pangolin results
-		cat $pipeline_batch_dir/pangolin/lineage_report.csv >> $merged_pangolin
+	    # # add to master pangolin results
+		# cat $pipeline_batch_dir/pangolin/lineage_report.csv >> $merged_pangolin
 
-		#add to master cecret summary
-		cat $pipeline_batch_dir/combined_summary.csv >> $merged_summary
+		# #add to master cecret summary
+		# cat $pipeline_batch_dir/combined_summary.csv >> $merged_summary
 
-		# If QC report is being created, generate stats on fragment length
-        for f in $pipeline_batch_dir/samtools_stats/*.stats.txt; do
-    		frag_length=`cat $f | grep "average length" | awk '{print $4}'`
-        	sampleid=`echo $f | rev | cut -f1 -d "/" | rev | cut -f1 -d "."`
-	        echo -e "${sampleid}\t${frag_length}\t${batch_id}" >> $merged_fragment
-    	done
+		# # If QC report is being created, generate stats on fragment length
+        # for f in $pipeline_batch_dir/samtools_stats/*.stats.txt; do
+    	# 	frag_length=`cat $f | grep "average length" | awk '{print $4}'`
+        # 	sampleid=`echo $f | rev | cut -f1 -d "/" | rev | cut -f1 -d "."`
+	    #     echo -e "${sampleid}\t${frag_length}\t${batch_id}" >> $merged_fragment
+    	# done
 
-		# move FASTQC files
-		mv $pipeline_batch_dir/fastqc/* $fastqc_dir
+		# # move FASTQC files
+		# mv $pipeline_batch_dir/fastqc/* $fastqc_dir
 		
-		# move FASTA files
-		mv $pipeline_batch_dir/consensus/*fa $fasta_dir/not_uploaded
-		for f in $fasta_dir/not_uploaded/*; do
-			new=`echo $f | sed "s/.consensus//g"`
-			mv $f $new
-		done
+		# # move FASTA files
+		# mv $pipeline_batch_dir/consensus/*fa $fasta_dir/not_uploaded
+		# for f in $fasta_dir/not_uploaded/*; do
+		# 	new=`echo $f | sed "s/.consensus//g"`
+		# 	mv $f $new
+		# done
 
-		#remove intermediate files
-		if [[ $flag_cleanup == "Y" ]]; then
-			sudo rm -r --force work
-			sudo rm -r --force */work
-			sudo rm -r --force $pipeline_batch_dir
-			sudo rm -r --force $fastq_batch_dir
-			cd ..
-			sudo rm -r $project_id
-		fi
+		# #remove intermediate files
+		# if [[ $flag_cleanup == "Y" ]]; then
+		# 	sudo rm -r --force work
+		# 	sudo rm -r --force */work
+		# 	sudo rm -r --force $pipeline_batch_dir
+		# 	sudo rm -r --force $fastq_batch_dir
+		# 	cd ..
+		# 	sudo rm -r $project_id
+		# fi
 	done
 fi
 
